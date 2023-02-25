890b083bf696561a690b80e69074aad22f4164d6
bowwow2115 <bowwow2115@gmail.com>
2023-02-25 12:21:00 +0900

    front controller 구현

--- src/main/java/com/example/helloboot/HelloController.java
+++ src/main/java/com/example/helloboot/HelloController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
 public class HelloController {
 
-    @GetMapping("/hello")
+//    @GetMapping("/hello")
     public String hello(String name) {
-         return "hello" + name;
+         return "hello " + name;
     }
 }
